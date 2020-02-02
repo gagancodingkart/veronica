14b0ca7d650ee965069414400b9700489b84ac99
gaganjain280 <gaganjain280@gmailcom>
2020-02-02 23:55:22 +0530

    feb commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {Component, Fragment, useState} from 'react';
+import './App.css';
+import Product from './components/Product';
+import { Col, Row } from 'react-bootstrap';
+import ProductSingle from './components/ProductSingle';
+import CategoryPage from './components/CategoryPage';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import Login from './components/Login';
+import Cart from './components/Cart';
+
+
+function App() {
+  
+  return (
+    <Router>
+    <Fragment>
+      <div className="App">
+      <h1 className="main-headiing">VERONICA</h1>
+          <Switch>
+          <Route exact path="/" component={Product}/>
+          <Route exact path="/login" component={Login}/>
+           <Route exact path="/cart" component={Cart}/>
+          <Route exact path="/product/:id" component={ProductSingle}/>
+          <Route exact path="/category/:id" component={CategoryPage}/>
+          </Switch>
+      
+    </div>
+    </Fragment>
+    </Router>
+  );
+}
+
+export default App;
